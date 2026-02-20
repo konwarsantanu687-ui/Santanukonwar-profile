2d38349a76c9e80d01b04146fada75b1e90d60f6
konwarsantanu687-ui <konwarsantanu687@gmail.com>
2026-02-20 19:22:01 +0530

    Update layout.tsx

--- App/layout.tsx
+++ App/layout.tsx
@@ -1,1 +1,21 @@
+import "./globals.css";
+import { initSmoothScroll } from "@/lib/smoothScroll";
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  if (typeof window !== "undefined") {
+    initSmoothScroll();
+  }
+
+  return (
+    <html lang="en">
+      <body className="bg-[#0b0f19] text-white overflow-x-hidden">
+        {children}
+      </body>
+    </html>
+  );
+}
