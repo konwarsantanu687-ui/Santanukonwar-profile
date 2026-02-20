543ce18f46a1f8bd47599c66aa5c1d480ae466fd
konwarsantanu687-ui <konwarsantanu687@gmail.com>
2026-02-20 19:21:34 +0530

    Update smoothScroll.ts

--- lib/smoothScroll.ts
+++ lib/smoothScroll.ts
@@ -1,1 +1,15 @@
+import Lenis from "lenis";
 
+export const initSmoothScroll = () => {
+  const lenis = new Lenis({
+    duration: 1.2,
+    smoothWheel: true,
+  });
+
+  function raf(time: number) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+};
